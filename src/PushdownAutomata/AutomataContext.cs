348b5fbc1130cacfaa4ac2bd8778cfc60e12b687
Victor Borges <victor1882@outlook.com>
2020-07-01 02:19:32 -0300

    fix: remove unnecessary class

--- src/PushdownAutomata/AutomataContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PushdownAutomation
-{
-    public struct AutomataContext
-    {
-        public int State { get; set; }
-        public char? InputSymbol { get; set; }
-        public char? StackSymbol { get; set; }
-    }
-}
